3097923becaeeff94271f2dc0e36c7918a3dc492
kstrinadka <k.strinadka@gmail.com>
2023-05-08 22:30:05 +0700

    created Spring project for application

---
+++ src/main/java/kstrinadka/residue_monitoring_system/ResidueMonitoringSystemApplication.java
@@ -0,0 +1,13 @@
+package kstrinadka.residue_monitoring_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidueMonitoringSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidueMonitoringSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kstrinadka/residue_monitoring_system/ResidueMonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kstrinadka.residue_monitoring_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidueMonitoringSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
